6485b33ffd8a0b8442476386829db1d3dcdac6cf
milica-bozic <milicabozic1997@gmail.com>
2021-07-19 11:21:54 +0200

    Catch!

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Hello Mika queen!");
+
+fetch("https://api.edamam.com/api/food-database/v2/parser?app_id=a1fa9480&app_key=fba8e90ad09d8cdc82794ffd61d58967&ingr=chocolate&nutrition-type=cooking")
+.then(response => {
+    if (response.ok) {
+        console.log("SUCCESS");
+    }
+    else {
+        console.log("NOT SUCCESSFUL");
+    }
+    response.json()
+})
+.then(data => console.log(data))
+.catch(error => console.log("ERROR", error))
