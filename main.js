1e6f05daf2c39bfad3c85f9fcea51db0af82d5aa
milica-bozic <milicabozic1997@gmail.com>
2021-07-19 14:43:10 +0200

    DOM creating element and adding border!

--- main.js
+++ main.js
@@ -1,12 +1,55 @@
-fetch("https://api.edamam.com/api/food-database/v2/parser?app_id=a1fa9480&app_key=fba8e90ad09d8cdc82794ffd61d58967&ingr=chocolate&nutrition-type=cooking")
-.then(response => {
-    if (response.ok) {
-        console.log("SUCCESS");
-    }
-    else {
-        console.log("NOT SUCCESSFUL");
-    }
-    return response.json()
-})
-.then(data => console.log(data))
-.catch(error => console.log("ERROR", error))
+const apiBaseUrl = "https://api.edamam.com/api/food-database/v2/parser";
+const appId = "a1fa9480";
+const appKey = "fba8e90ad09d8cdc82794ffd61d58967";
+
+function searchProducts(keyword) {
+    fetch(`${apiBaseUrl}?app_id=${appId}&app_key=${appKey}&ingr=${keyword}&nutrition-type=cooking`)
+    .then(response => {
+        if (response.ok) {
+            console.log("SUCCESS");
+        }
+        else {
+            console.log("NOT SUCCESSFUL");
+        }
+        return response.json();
+    })
+    .then(data => drawProducts(data))
+    .catch(error => console.log("ERROR", error));
+}
+
+searchProducts("avocado");
+
+searchProducts("chocolate");
+
+function drawProducts(data) {
+    console.log(data);
+    let foodContainerDiv = document.createElement("div");
+    foodContainerDiv.classList.add("radio");
+    
+    data.hints.forEach(elem => {
+
+        let foodDiv = document.createElement("div");
+        foodDiv.classList.add("foodBorder");
+        foodContainerDiv.appendChild(foodDiv);
+        
+
+        let foodNameParagraph = document.createElement("p");
+
+        foodNameParagraph.textContent = elem.food.label;
+
+        foodDiv.appendChild(foodNameParagraph);
+
+        let foodImage = document.createElement("img");
+
+
+
+        foodImage.src = elem.food.image;
+
+        foodImage.alt = elem.food.label;
+
+        foodDiv.appendChild(foodImage);
+    });
+
+    document.body.appendChild(foodContainerDiv);
+}
+
