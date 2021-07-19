95f45d708cd93da0a6d54891caf9e4b998c670d0
milica-bozic <milicabozic1997@gmail.com>
2021-07-19 11:32:46 +0200

    return!

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("Hello Mika queen!");
-
 fetch("https://api.edamam.com/api/food-database/v2/parser?app_id=a1fa9480&app_key=fba8e90ad09d8cdc82794ffd61d58967&ingr=chocolate&nutrition-type=cooking")
 .then(response => {
     if (response.ok) {
@@ -8,7 +6,7 @@ fetch("https://api.edamam.com/api/food-database/v2/parser?app_id=a1fa9480&app_ke
     else {
         console.log("NOT SUCCESSFUL");
     }
-    response.json()
+    return response.json()
 })
 .then(data => console.log(data))
 .catch(error => console.log("ERROR", error))
